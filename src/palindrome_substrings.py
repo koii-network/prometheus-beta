dad043ae74fea77bff2a13898e00d6b7d59502cf
Cris91-code <Cris91-code@users.noreply.github.com>
2025-02-19 21:47:12 +0000

    Final optimized implementation of shortest palindromic substrings

--- src/palindrome_substrings.py
+++ src/palindrome_substrings.py
@@ -11,32 +11,26 @@ def find_shortest_palindromic_substrings(s):
     if not s:
         return []
     
-    # Track palindromes at each length
-    palindrome_lengths = {}
+    # Track palindromes
+    palindromes = {1: {c for c in s}}
+    max_palindrome_length = 1
     
-    # Search for palindromes of increasing length
-    for length in range(1, len(s) + 1):
-        # Find palindromes of current length
+    # Find palindromes of increasing length
+    for length in range(2, len(s) + 1):
         current_palindromes = set()
         for i in range(len(s) - length + 1):
             substr = s[i:i+length]
             if substr == substr[::-1]:
                 current_palindromes.add(substr)
         
-        # If palindromes found, store and check
+        # If palindromes found, store and update max length
         if current_palindromes:
-            palindrome_lengths[length] = current_palindromes
+            palindromes[length] = current_palindromes
+            max_palindrome_length = length
             
-            # If we find palindromes of length > 1, we can stop
+            # If we've found multiple length palindromes, we can stop
             if length > 1:
                 break
     
-    # If no palindromes found, return single-character palindromes
-    if not palindrome_lengths:
-        return list(find_shortest_palindromic_substrings(s[0]))
-    
-    # Find the minimum lengths
-    min_length = min(palindrome_lengths.keys())
-    
-    # Return all shortest palindromes
-    return list(palindrome_lengths[min_length])
+    # Return the palindromes of the shortest found length
+    return list(palindromes[max_palindrome_length])
