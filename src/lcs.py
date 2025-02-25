776efdeef23e28926e703d6e0148808a54a4de44
Iulian130282 <Iulian130282@users.noreply.github.com>
2025-02-25 22:19:20 +0000

    Update LCS implementation to handle case sensitivity and multiple subsequences

--- src/lcs.py
+++ src/lcs.py
@@ -23,6 +23,10 @@ def longest_common_subsequence(str1: str, str2: str) -> str:
     if not isinstance(str1, str) or not isinstance(str2, str):
         raise TypeError("Inputs must be strings")
     
+    # Case-sensitive check
+    str1 = str1
+    str2 = str2
+    
     # If either string is empty, return empty string
     if not str1 or not str2:
         return ""
@@ -39,18 +43,31 @@ def longest_common_subsequence(str1: str, str2: str) -> str:
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     
-    # Reconstruct the LCS
-    lcs = []
-    i, j = m, n
-    while i > 0 and j > 0:
+    # Reconstruct ALL possible LCS
+    def find_all_lcs(i, j, current_lcs):
+        # Base case: reached the start of either string
+        if i == 0 or j == 0:
+            return [current_lcs[::-1]]
+        
+        # If characters match, include in LCS
         if str1[i-1] == str2[j-1]:
-            lcs.append(str1[i-1])
-            i -= 1
-            j -= 1
-        elif dp[i-1][j] > dp[i][j-1]:
-            i -= 1
-        else:
-            j -= 1
+            return find_all_lcs(i-1, j-1, current_lcs + str1[i-1])
+        
+        # If not matching, explore both possible paths
+        result = []
+        if dp[i-1][j] >= dp[i][j-1]:
+            result.extend(find_all_lcs(i-1, j, current_lcs))
+        if dp[i][j-1] >= dp[i-1][j]:
+            result.extend(find_all_lcs(i, j-1, current_lcs))
+        
+        return result
     
-    # Return the LCS in the correct order
-    return ''.join(reversed(lcs))
+    # Get all possible LCS
+    all_lcs = find_all_lcs(m, n, '')
+    
+    # If no LCS found, return empty string
+    if not all_lcs:
+        return ""
+    
+    # Return the lexicographically first LCS of max length
+    return min(filter(lambda x: len(x) == len(max(all_lcs, key=len)), all_lcs))
